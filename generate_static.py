48408899b07998aafdea18bb45578f51b2bdef5b
elnukakujo <jagernoe1s3@hotmail.com>
2024-06-15 22:59:38 -0400

    Test Website

--- generate_static.py
+++ generate_static.py
@@ -1,14 +1,12 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
-from dash.dependencies import Input, Output, State
-import dash_renderer
 import preprocess
 import make_viz
 import plotly.io as pio
 import hovertemplate as hover
 
-# Create the Dash app
+# Initialize Dash app
 app = dash.Dash(__name__)
 app.title = 'SportsAI Project'
 
@@ -19,40 +17,8 @@ pio.templates.default = 'simple_white'
 fig1 = make_viz.create_scatter(data, hover.get_scatter_hover_template())
 fig2 = make_viz.create_stacked_bars(data, hover.get_stacked_bar_hover_template)
 
-app.layout = html.Div([
-    html.H1('SportsAI Project'),
-    html.Div(className='viz-container', children=[
-        dcc.Graph(
-            figure=fig1,
-            config=dict(
-                scrollZoom=False,
-                showTips=False,
-                showAxisDragHandles=False,
-                doubleClick=False,
-                displayModeBar=False
-            ),
-            className='graph',
-            id='scatter-plot'
-        )
-    ]),
-    html.Div(className='viz-container', children=[
-        dcc.Graph(
-            figure=fig2,
-            config=dict(
-                scrollZoom=False,
-                showTips=False,
-                showAxisDragHandles=False,
-                doubleClick=False,
-                displayModeBar=False
-            ),
-            className='graph',
-            id='stacked-bar'
-        )
-    ])
-])
-
-# Generate static HTML for the layout
-html_string = '''
+# Define HTML for components
+html_content = '''
 <!DOCTYPE html>
 <html>
 <head>
@@ -61,11 +27,25 @@ html_string = '''
     <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
-{content}
+    <h1>SportsAI Project</h1>
+    
+    <div class="viz-container">
+        <div class="graph">
+            {fig1}
+        </div>
+    </div>
+
+    <div class="viz-container">
+        <div class="graph">
+            {fig2}
+        </div>
+    </div>
+
+    <!-- Include any JavaScript files here if needed -->
 </body>
 </html>
-'''.format(content=app.layout)
+'''.format(fig1=fig1.to_html(), fig2=fig2.to_html())
 
 # Save the static HTML
 with open("index.html", "w") as file:
-    file.write(html_string)
+    file.write(html_content)
