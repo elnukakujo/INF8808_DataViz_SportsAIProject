4ce0708e7c34a09f47303814cfbe5253aa4c8020
ArmanLidder <arman.lidder@polymtl.ca>
2024-06-18 20:27:55 -0400

    Added radar chart with team choosing functionality

---
+++ src/Arman/make_viz.py
@@ -0,0 +1,123 @@
+import plotly.graph_objects as go
+
+
+def create_radar_chart(merged_stats, team):
+    team_stats = merged_stats[merged_stats['TeamName'] == team].iloc[0]
+
+    categories = ['Goals Scored per Game',
+                  'Attempts Accuracy',
+                  'Passes Accuracy',
+                  'Fouls Commited per Game',
+                  'Ball Possession % per Game']
+
+    values = [team_stats['Scaled Value Goals'],
+              team_stats['Average Attempts % Accuracy'],
+              team_stats['Average Passes Accuracy'],
+              team_stats['Scaled Value Fouls'],  # Inverting fouls for better visualization
+              team_stats['Average Ball Possession % per Game']]
+
+    actual_values = [team_stats['Average Goals Scored per Game'],
+                     team_stats['Average Attempts % Accuracy'],
+                     team_stats['Average Passes Accuracy'],
+                     team_stats['Average Fouls Commited per Game'],
+                     team_stats['Average Ball Possession % per Game']]
+
+    hover_template = (
+        "<b>%{theta}</b><br>"
+        "<b>Scaled Value: </b>%{r:.2f}<br>"
+        "<b>Actual Value: </b>%{customdata:.2f}<extra></extra>"
+    )
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatterpolar(
+        r=values,
+        theta=categories,
+        fill='toself',
+        name=team,
+        customdata=actual_values,
+        hovertemplate=hover_template,
+        line=dict(color='blue'),
+    ))
+
+    fig.update_layout(
+        polar=dict(
+            radialaxis=dict(
+                visible=True,
+                showline=True,
+                showgrid=True,
+                gridcolor='gray',
+                range=[0, 1]
+            )),
+        showlegend=True,
+        legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="right",
+            x=1
+        ),
+        title=f'Team Comparison Radar Chart',
+        hoverdistance=5
+    )
+
+    return fig
+
+def add_team_to_radar_chart(fig, merged_stats, second_team):
+    team_stats = merged_stats[merged_stats['TeamName'] == second_team].iloc[0]
+
+    categories = ['Goals Scored per Game',
+                  'Attempts % Accuracy',
+                  'Passes % Accuracy',
+                  'Fouls Commited per Game',
+                  'Ball Possession % per Game']
+
+    values = [team_stats['Scaled Value Goals'],
+              team_stats['Average Attempts % Accuracy'],
+              team_stats['Average Passes Accuracy'],
+              team_stats['Scaled Value Fouls'],
+              team_stats['Average Ball Possession % per Game']]
+
+    actual_values = [team_stats['Average Goals Scored per Game'],
+                     team_stats['Average Attempts % Accuracy'],
+                     team_stats['Average Passes Accuracy'],
+                     team_stats['Average Fouls Commited per Game'],
+                     team_stats['Average Ball Possession % per Game']]
+
+    hover_template = (
+        "<b>%{theta}</b><br>"
+        "<b>Scaled Value: </b> %{r:.2f}<br>"
+        "<b>Actual Value: </b>%{customdata:.2f}<extra></extra>"
+    )
+
+    fig.add_trace(go.Scatterpolar(
+        r=values,
+        theta=categories,
+        fill='toself',
+        name=second_team,
+        customdata=actual_values,
+        hovertemplate=hover_template,
+        line=dict(color='red')
+    ))
+
+    return fig
+
+def create_empty_radar_chart():
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatterpolar(
+    ))
+    fig.update_layout(
+        polar=dict(
+            radialaxis=dict(
+                visible=True,
+                showline=True,
+                showgrid=True,
+                gridcolor='gray',
+                range=[0, 1]
+            )),
+        title=f'Empty Radar Chart (Select a team)',
+        hoverdistance=5,
+        dragmode = False
+    )
+    return fig
--- src/app.py
+++ src/app.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 
 '''
@@ -10,20 +9,22 @@
     This file is the entry point for our dash app.
 '''
 
-
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
 import plotly.io as pio
+from dash import Input, Output
+import plotly.graph_objects as go
 
 app = dash.Dash(__name__)
 app.title = 'SportsAI Project'
 
 import get_df
+
 match_infos = get_df.get_df('Match_information')
 match_stats = get_df.get_df('Match_Stats')
 player_stats = get_df.get_df('Players_stats')
-line_ups= get_df.get_df('Line-ups')
+line_ups = get_df.get_df('Line-ups')
 
 pio.templates.default = 'simple_white'
 
@@ -40,157 +41,230 @@ import Abdel.preprocess as abdel_preprocess
 import Abdel.make_viz as abdel_makeviz
 
 df1, df2 = abdel_preprocess.preprocess(player_stats, line_ups)
-fig3,fig4 = abdel_makeviz.create_bars(df1, df2)
+fig3, fig4 = abdel_makeviz.create_bars(df1, df2)
 
 import Amadeus.preprocess as amadeus_preprocess
 import Amadeus.make_viz as amadeus_makeviz
 
 df1, df2, df3 = amadeus_preprocess.preprocess(player_stats, line_ups)
-fig5,fig6,fig7 = amadeus_makeviz.draw(df1, df2, df3)
+fig5, fig6, fig7 = amadeus_makeviz.draw(df1, df2, df3)
+
+import Arman.preprocess as arman_preprocess
+import Arman.make_viz as arman_makeviz
+
+radar_data = arman_preprocess.preprocess(match_stats)
+# fig8 = arman_makeviz.create_radar_chart(radar_data, 'Italy')
 
 app.layout = html.Div([
-    html.Div(className='anchor',id='0'),
-    html.Div(className='intro',children=[
+    html.Div(className='anchor', id='0'),
+    html.Div(className='intro', children=[
         html.H2('SportsAI'),
         html.H1('Boost your Performance with Data'),
     ]),
-    html.Div(className='anchor',id='1'),
+    html.Div(className='anchor', id='1'),
     html.Div(className='about_us', children=[
         html.P(
-            "At SportsAI, we are dedicated to revolutionizing the way coaches enhance their players' "+
-            "performance through cutting-edge data visualization. Our team combines expertise in sports "+
-            "science and advanced analytics to provide insightful, actionable data that empowers coaches to "+
-            "make informed decisions. Whether it's tracking progress, identifying strengths and weaknesses, or "+ 
-            "optimizing training strategies, SportsAI is your partner in achieving peak performance. Join us and "+
+            "At SportsAI, we are dedicated to revolutionizing the way coaches enhance their players' " +
+            "performance through cutting-edge data visualization. Our team combines expertise in sports " +
+            "science and advanced analytics to provide insightful, actionable data that empowers coaches to " +
+            "make informed decisions. Whether it's tracking progress, identifying strengths and weaknesses, or " +
+            "optimizing training strategies, SportsAI is your partner in achieving peak performance. Join us and " +
             "transform your data into a competitive edge."
         ),
-        html.Div(className='img-container',children=[
+        html.Div(className='img-container', children=[
             html.Img(src='assets/img/aboutus.jpg', alt='A coach talking to her team'),
-            html.A('Image Source', href='https://www.dickssportinggoods.com/protips/sports-and-activities/soccer/soccer-coaching-tips-game-day-prep', target='_blank')
+            html.A('Image Source',
+                   href='https://www.dickssportinggoods.com/protips/sports-and-activities/soccer/soccer-coaching-tips-game-day-prep',
+                   target='_blank')
         ])
     ]),
     html.Div(className='match-overview', children=[
         html.H3('Match Overview'),
         html.Div(className='description', children=[
             html.P(
-                "The first group of visualizations includes multiple graphics that illustrate various aspects of the matches. "+
-                "These visualizations are designed to address general questions that users might have and to introduce them to "+
+                "The first group of visualizations includes multiple graphics that illustrate various aspects of the matches. " +
+                "These visualizations are designed to address general questions that users might have and to introduce them to " +
                 "our project. Specifically, we present:"
             ),
             html.Ol(className='fig-description', children=[
-                html.Li("The number of goals scored and fouls committed in each match, under the assumption that the entertainment "+
-                        "value of a game is correlated with the number of goals."
+                html.Li(
+                    "The number of goals scored and fouls committed in each match, under the assumption that the entertainment " +
+                    "value of a game is correlated with the number of goals."
                 ),
-                html.Li("A comparison between the number of goals scored in open play and those scored from set pieces.")
+                html.Li(
+                    "A comparison between the number of goals scored in open play and those scored from set pieces.")
             ]),
             html.P(
-                "These visualizations offer a comprehensive overview of key match statistics, enhancing the understanding "+
+                "These visualizations offer a comprehensive overview of key match statistics, enhancing the understanding " +
                 "of the factors that contribute to the dynamics of the game."
             ),
             html.P("Try hovering hover the elements in the graph to get more details!")
         ]),
         html.Div(className='viz-container', children=[
             dcc.Graph(
-                        figure=fig1,
-                        config=dict(
-                            scrollZoom=False,
-                            showTips=False,
-                            showAxisDragHandles=False,
-                            doubleClick=False,
-                            displayModeBar=False
-                        ),
-                        className='graph',
-                        id='scatter-plot'
-                    ),
+                figure=fig1,
+                config=dict(
+                    scrollZoom=False,
+                    showTips=False,
+                    showAxisDragHandles=False,
+                    doubleClick=False,
+                    displayModeBar=False
+                ),
+                className='graph',
+                id='scatter-plot'
+            ),
             dcc.Graph(
-                    figure=fig2,
-                    config=dict(
-                        scrollZoom=False,
-                        showTips=False,
-                        showAxisDragHandles=False,
-                        doubleClick=False,
-                        displayModeBar=False
-                    ),
-                    className='graph',
-                    id='stacked-bar'
-                )
+                figure=fig2,
+                config=dict(
+                    scrollZoom=False,
+                    showTips=False,
+                    showAxisDragHandles=False,
+                    doubleClick=False,
+                    displayModeBar=False
+                ),
+                className='graph',
+                id='stacked-bar'
+            )
         ])
     ]),
-    html.Div(className='anchor',id='2'),
+    html.Div(className='anchor', id='2'),
     html.Div(className='viz-container', children=[
         dcc.Graph(
-                    figure=fig3,
-                    config=dict(
-                        scrollZoom=False,
-                        showTips=False,
-                        showAxisDragHandles=False,
-                        doubleClick=False,
-                        displayModeBar=False
-                    ),
-                    className='graph',
-                    id='bar1'
-                )
+            figure=fig3,
+            config=dict(
+                scrollZoom=False,
+                showTips=False,
+                showAxisDragHandles=False,
+                doubleClick=False,
+                displayModeBar=False
+            ),
+            className='graph',
+            id='bar1'
+        )
     ]),
-    html.Div(className='anchor',id='3'),
+    html.Div(className='anchor', id='3'),
     html.Div(className='viz-container', children=[
         dcc.Graph(
-                    figure=fig4,
-                    config=dict(
-                        scrollZoom=False,
-                        showTips=False,
-                        showAxisDragHandles=False,
-                        doubleClick=False,
-                        displayModeBar=False
-                    ),
-                    className='graph',
-                    id='bar2'
-                )
+            figure=fig4,
+            config=dict(
+                scrollZoom=False,
+                showTips=False,
+                showAxisDragHandles=False,
+                doubleClick=False,
+                displayModeBar=False
+            ),
+            className='graph',
+            id='bar2'
+        )
     ]),
-    html.Div(className='anchor',id='4'),
+    html.Div(className='anchor', id='4'),
     html.Div(id='pie-charts', children=[
         html.Div(className='viz-container', children=[
             dcc.Graph(
-                        figure=fig5,
-                        config=dict(
-                            scrollZoom=False,
-                            showTips=False,
-                            showAxisDragHandles=False,
-                            doubleClick=False,
-                            displayModeBar=False
-                        ),
-                        className='graph',
-                        id='pie1'
-                    ),
+                figure=fig5,
+                config=dict(
+                    scrollZoom=False,
+                    showTips=False,
+                    showAxisDragHandles=False,
+                    doubleClick=False,
+                    displayModeBar=False
+                ),
+                className='graph',
+                id='pie1'
+            ),
         ]),
         html.Div(className='viz-container', children=[
             dcc.Graph(
-                        figure=fig6,
-                        config=dict(
-                            scrollZoom=False,
-                            showTips=False,
-                            showAxisDragHandles=False,
-                            doubleClick=False,
-                            displayModeBar=False
-                        ),
-                        className='graph',
-                        id='pie2'
-                    ),
+                figure=fig6,
+                config=dict(
+                    scrollZoom=False,
+                    showTips=False,
+                    showAxisDragHandles=False,
+                    doubleClick=False,
+                    displayModeBar=False
+                ),
+                className='graph',
+                id='pie2'
+            ),
         ]),
         html.Div(className='viz-container', children=[
             dcc.Graph(
-                        figure=fig7,
-                        config=dict(
-                            scrollZoom=False,
-                            showTips=False,
-                            showAxisDragHandles=False,
-                            doubleClick=False,
-                            displayModeBar=False
-                        ),
-                        className='graph',
-                        id='pie3'
-                    )
+                figure=fig7,
+                config=dict(
+                    scrollZoom=False,
+                    showTips=False,
+                    showAxisDragHandles=False,
+                    doubleClick=False,
+                    displayModeBar=False
+                ),
+                className='graph',
+                id='pie3'
+            )
+        ])
+    ]),
+    html.Div(className='anchor', id='5'),
+    html.Div([
+        html.Div(className='radar-chart-section', children=[
+            html.Div(className='radar-chart-explanation', children=[
+                html.H3('Team Performance Radar Chart'),
+                html.P(
+                    "The radar chart compares various performance metrics across teams. The first team is defaulted to Italy, the winner of the tournament. Each axis represents a different statistic: "
+                ),
+                html.Ul([
+                    html.Li("Goals Scored per Game: Average number of goals scored in each game. Note: This value is MinMax Scaled to [0,1] range."),
+                    html.Li("Attempts Accuracy: Percentage of attempts on target."),
+                    html.Li("Passes Accuracy: Percentage of successful passes."),
+                    html.Li("Fouls Committed per Game: Average number of fouls committed per game. Note: This value is MinMax Scaled to [0,1] range."),
+                    html.Li(
+                        "Ball Possession % per Game: Average percentage of time the team has possession of the ball.")
+                ]),
+                html.Label('Select First Team'),
+                dcc.Dropdown(
+                    id='first-team-dropdown',
+                    options=[{'label': team, 'value': team} for team in radar_data['TeamName']],
+                    value='Italy'
+                ),
+                html.Label('Select Second Team'),
+                dcc.Dropdown(
+                    id='second-team-dropdown',
+                    options=[{'label': team, 'value': team} for team in radar_data['TeamName']]
+                )
+            ]),
+            html.Div(className='radar-chart-container', children=[
+                dcc.Graph(
+                    id='radar-chart',
+                    config=dict(
+                        scrollZoom=True,
+                        showTips=False,
+                        showAxisDragHandles=False,
+                        doubleClick=False,
+                        displayModeBar=False
+                    ),
+                    className='radar-chart'
+                )
+            ])
         ])
     ])
 ])
+
+@app.callback(
+    Output('radar-chart', 'figure'),
+    Input('first-team-dropdown', 'value'),
+    Input('second-team-dropdown', 'value')
+)
+def update_radar_chart(first_team, second_team):
+    if first_team and second_team:
+        radar_fig = arman_makeviz.create_radar_chart(radar_data, first_team)
+        arman_makeviz.add_team_to_radar_chart(radar_fig, radar_data, second_team)
+        return radar_fig
+    if not first_team and not second_team:
+        return arman_makeviz.create_empty_radar_chart()
+    if not first_team:
+        radar_fig = arman_makeviz.create_radar_chart(radar_data, second_team)
+        return radar_fig
+    if not second_team:
+        radar_fig = arman_makeviz.create_radar_chart(radar_data, first_team)
+        return radar_fig
+
 
 server = app.server
