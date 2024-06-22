61a01613b45a289e96e8bdf05634fd025edbddbb
OmarAlaaeldein <omar.alaaeldein@ejust.edu.eg>
2024-06-21 20:08:06 -0400

    change amadeus plot from pie to bar

--- src/Amadeus/make_viz.py
+++ src/Amadeus/make_viz.py
@@ -7,38 +7,36 @@ role_colors = {
     "goalkeepers": "#AB63FA"
 }
 
-def prepare_pie_data(series):
+def prepare_bar_data(series):
     sorted_series = series.sort_values(ascending=True)
-    total = sorted_series.sum()
     labels = sorted_series.index
     values = sorted_series.values
-    return labels, values, total
+    return labels, values
 
-def create_pie_chart(labels, values, title):
-    fig = go.Figure(data=[go.Pie(labels=labels, values=values, sort=False)])
+def create_bar_chart(labels, values, title):
+    fig = go.Figure(data=[go.Bar(x=labels, y=values, marker=dict(color=[role_colors[label] for label in labels]))])
     fig.update_traces(
-        textposition='inside',
-        textinfo='percent+label',
-        marker=dict(colors=[role_colors[label] for label in labels]),
-        hovertemplate='<b>%{label}</b><br>Total: %{value}<br>Percentage: {percent}%<extra></extra>'
+        hovertemplate='<b>%{x}</b><br>Total: %{y}<extra></extra>'
     )
     fig.update_layout(
         title_text=title,
         margin=dict(t=50, b=50, l=50, r=50),
         height=600,
         width=600,
-        showlegend=True,
+        showlegend=False,
+        xaxis_title="Roles",
+        yaxis_title="Counts",
     )
     return fig
 
-def draw(df1,df2,df3):
-    labels, values, total = prepare_pie_data(df1)
-    fig_contributions = create_pie_chart(labels, values, "Contributions by Role")
+def draw(df1, df2, df3):
+    labels, values = prepare_bar_data(df1)
+    fig_contributions = create_bar_chart(labels, values, "Contributions by Role")
     
-    labels, values, total = prepare_pie_data(df2)
-    fig_goals = create_pie_chart(labels, values, "Goals by Role")
+    labels, values = prepare_bar_data(df2)
+    fig_goals = create_bar_chart(labels, values, "Goals by Role")
     
-    labels, values, total = prepare_pie_data(df3)
-    fig_assists = create_pie_chart(labels, values, "Assists by Role")
+    labels, values = prepare_bar_data(df3)
+    fig_assists = create_bar_chart(labels, values, "Assists by Role")
     
-    return fig_contributions,fig_goals,fig_assists
+    return fig_contributions, fig_goals, fig_assists
