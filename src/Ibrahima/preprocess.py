896f9979765a09d7bb0a6e2bacde076847bdcab9
elnukakujo <jagernoe1s3@hotmail.com>
2024-06-25 21:58:19 -0400

    Fixed Ibrahima preprocess

--- src/Ibrahima/preprocess.py
+++ src/Ibrahima/preprocess.py
@@ -1,9 +1,8 @@
 def preprocess(players_stats):
     players_stats = players_stats[["FullName", "StatsID", "StatsName", "Value"]]
     players_stats = players_stats.query("StatsID == 17")
+    players_stats['Value'] = players_stats['Value'].astype('int')
     
-    tackles_stats = players_stats.groupby('FullName')['Value'].sum().reset_index()
-    
-    tackles_stats['Value'] = tackles_stats['Value'].astype('int')
+    tackles_stats = players_stats.groupby('FullName')['Value'].sum().reset_index()    
     
     return tackles_stats.nlargest(15, 'Value')
